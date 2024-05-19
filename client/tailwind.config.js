a3aeea2b1dbef3cca58fc4fb94011d4eecc220ea
ceyx0001 <45667885+ceyx0001@users.noreply.github.com>
2024-05-19 04:01:13 -0400

    nav bar ui complete

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,11 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
     extend: {
       fontFamily: {
-        roboto: ["Roboto", "sans-serif"],
+        roboto: ["Inter", "sans-serif"],
+      },
+      colors: {
+        text: "#f2f0f3",
+        background: "#0e0c0f",
+        primary: "#b6abbe",
+        secondary: "#5d484b",
+        accent: "#99827b",
+      },
+      borderWidth: {
+        1: "1px",
       },
     },
   },
